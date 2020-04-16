4c8227d59c776d4175ba83652f8b3e6214f6115b
wonix <2925321994@qq.com>
2020-04-16 11:33:17 +0800

    20200416 新建练习项目

---
+++ src/test/java/com/wonix/exercise/ExerciseApplicationTests.java
@@ -0,0 +1,15 @@
+package com.wonix.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
